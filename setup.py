aea785349f1ab6f7739a55347af57fa680f3151d
Christopher Edmonds <tunecrew@users.noreply.github.com>
2024-12-10 13:27:16 -0500

    Update setup.py
    changes from https://github.com/klen/pylama/pull/254/commits/e34d6d4caec7e6bee1a8fe2496e5673c5e1a9016

--- setup.py
+++ setup.py
@@ -4,22 +4,32 @@
 
 import pathlib
 
-import pkg_resources
 from setuptools import setup
-
-
-def parse_requirements(path: str) -> 'list[str]':
-    with pathlib.Path(path).open(encoding='utf-8') as requirements:
-        return [str(req) for req in pkg_resources.parse_requirements(requirements)]
-
 
 OPTIONAL_LINTERS = ['pylint', 'eradicate', 'radon', 'mypy', 'vulture']
 
 
 setup(
-    install_requires=parse_requirements('requirements/requirements.txt'),
+    install_requires=[
+        'mccabe>=0.7.0',
+        'pycodestyle>=2.9.1',
+        'pydocstyle>=6.1.1',
+        'pyflakes>=2.5.0'
+    ],
     extras_require=dict(
-        tests=parse_requirements('requirements/requirements-tests.txt'),
+tests=[
+            'pytest>=7.1.2',
+            'pytest-mypy',
+            'eradicate>=2.0.0',
+            'radon>=5.1.0',
+            'mypy',
+            'pylint>=2.11.1',
+            'pylama-quotes',
+            'toml',
+            'vulture',
+            'types-setuptools',
+            'types-toml'
+        ],
         all=OPTIONAL_LINTERS, **{linter: [linter] for linter in OPTIONAL_LINTERS},
         toml='toml>=2.0.1',
     ),
